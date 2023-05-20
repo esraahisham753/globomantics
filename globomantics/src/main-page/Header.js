efbbfaca003537e69f7587ced9951df866300d8d
Esraa Abduallah <esraahisham753@gmail.com>
2023-05-20 17:10:20 +0000

    start create header component

---
+++ globomantics/src/main-page/Header.js
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div className="row">
+                <div className="col-md-5">
+                    
+                </div>
+            </div> 
+        </header>
+    )
+}
