5ae99cfa90eb146d3321cebc16ea6a65ff942445
Esraa Abduallah <esraahisham753@gmail.com>
2023-05-21 15:09:49 +0000

    caching Featured House

--- globomantics/src/main-page/index.js
+++ globomantics/src/main-page/index.js
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useEffect, useState, useMemo} from 'react';
 
 import './main-page.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,13 +6,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
 
 function App() {
+  const [houses, setHouses] = useState([]);
+
   useEffect(() => {
     const fetchHouses = async () => {
       const resp = await fetch("/houses.json");
       const houses = await resp.json();
+      setHouses(houses);
     };
     fetchHouses();
   }, []);
+
+  const featuredHouse = useMemo(() => {
+    if(houses.length) {
+      const randomIndex = Math.floor(Math.random() * houses.length);
+      return houses[randomIndex];
+    }
+  }, [houses]);
 
   return (
     <div className="container">
