ec190b7e8804e6cc9fca5778934682be890a09f5
Esraa Abduallah <esraahisham753@gmail.com>
2023-05-21 12:45:10 +0000

    fetch houses data

--- globomantics/src/main-page/Header.js
+++ globomantics/src/main-page/Header.js
@@ -1,12 +1,19 @@
 
-const Header = () => {
+import logo from './GloboLogo.png';
+
+const Header = ({title}) => {
     return (
         <header>
             <div className="row">
-                <div className="col-md-5">
-                    
+                <div className="col-md-5 logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div className="col-md-7 title">
+                    <h1>{title}</h1>
                 </div>
             </div> 
         </header>
     )
 }
+
+export default Header;
--- globomantics/src/main-page/index.js
+++ globomantics/src/main-page/index.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import {useEffect} from 'react';
+
 import './main-page.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Header from './Header';
+
 function App() {
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const resp = await fetch("/houses.json");
+      const houses = await resp.json();
+    };
+    fetchHouses();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Providing houses all over the world" />
     </div>
   );
 }
